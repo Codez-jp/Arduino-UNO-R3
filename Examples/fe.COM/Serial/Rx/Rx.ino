238ab11a02d2685119bb3d79204155d963e4e2d2
Makoto Hasunuma <makot@codez.jp>
2023-10-14 21:39:46 +0900

    tipo Serial

--- Examples/fe.COM/Serial/Rx/Rx.ino
+++ Examples/fe.COM/Serial/Rx/Rx.ino
@@ -1,6 +1,6 @@
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup( ){
+  Serial.begin( 9600 );
+  pinMode( LED_BUILTIN, OUTPUT );
 }
 
 // 受信バイト数をクリア
@@ -8,18 +8,18 @@ int incomingByte = 0;
 
 void loop() {
   // シリアルが有効な場合
-  if (Serial.available() > 0) {
+  if( Serial.available( ) > 0 ){
     // データを１バイト読み込む
-    incomingByte = Serial.read();
+    incomingByte = Serial.read( );
     // 受信データを10進数で送信する
-    Serial.print("0d");
-    Serial.println(incomingByte, DEC);
+    Serial.print( "0d" );
+    Serial.println( incomingByte, DEC );
 
     // LEDを点灯
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite( LED_BUILTIN, HIGH );
     // 100ms点灯を保持
-    delay(100);
+    delay( 100 );
     // LEDを消灯
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite( LED_BUILTIN, LOW );
   }
 }
--- Examples/fe.COM/Serial/Tx/Tx.ino
+++ Examples/fe.COM/Serial/Tx/Tx.ino
@@ -1,26 +1,26 @@
 void setup() {
   // ボーレート:9600bps, データ長:8bit, パリティ:無し, ストップビット:1bit
-  Serial.begin(9600, SERIAL_8N1);
+  Serial.begin( 9600, SERIAL_8N1 );
   // ボード上に実装済みのLEDを出力に設定 ※LED_BUILTINは13と同値
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode( LED_BUILTIN, OUTPUT );
 }
 
 
 // カウンタをクリア
 int counter = 0;
 
-void loop() {
+void loop( ){
   // カウンタ値を送信
-  Serial.write(counter);
+  Serial.write( counter );
 
   // LEDを点灯
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite( LED_BUILTIN, HIGH );
   // 100msスリープ
-  delay(100);
+  delay( 100 );
   // LEDを消灯
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite( LED_BUILTIN, LOW );
   // 100msスリープ
-  delay(900);
+  delay( 900 );
   // カウンタをインクリメント
   counter++;
 }
